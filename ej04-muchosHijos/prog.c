e83eed231ea190d1cff9ce928b7b29f60975300a
santichulo <150003@iessierradeguara.com>
2022-09-30 10:16:14 +0200

    anotacion de explicaciones

--- ej04-muchosHijos/prog.c
+++ ej04-muchosHijos/prog.c
@@ -14,19 +14,24 @@ int main (int argc, char const *argv[]){
             //Codigo de los hijos
             printf("Hola soy el hijo número: %d.\n", getpid());
             sleep(1);
-            return i; //Ponemos un return i para que los hijos mueran aqui y no sigan por abajo recorriendo el codigo
+            return i; //Ponemos un return i para que los hijos obtengan un valor de retorno y no sigan abajo si hubiera un else
                         //Ponemos i para que se queden como valor de retorno en la vuelta en la que estaban
         }
     }
 
     int estado;
     int pidHijo;
-    //El wait devuelve el codigo pid del hijo que finaliza
+    //La funcion wait() va asociada a la funcion fork(), wait espera a que el/los procesos hijos acaben, si asocias wait() a 
+    //una variable, te devolverá el id del hijo que ha esperado
     for (int i = 0; i < N_HIJOS; i++)
     {
-        pidHijo = wait(&estado);
-        printf("El hijo %d ha finalizado con valor de retorno %d\n",
-        pidHijo, WEXITSTATUS(estado));
+        pidHijo = wait(&estado); //Con esto ademas de guardar el pid del hijo que haya llegado en la 
+        //variable pidHijo, se guarda en la variable estado el byte de la posicion que ocupa en cada vuelta y para cada hijo
+        printf("El hijo %d ha finalizado con estado %d y valor de retorno %d\n",
+        pidHijo, estado,WEXITSTATUS(estado));
+
+        //la funcion WEXITSTATUS(estado) busca dentro de los bytes que ocupa, justo en los 8 ultimos bytes el estado en el que se encuentra, 
+        //en este caso como los hijos tienen un return guardado en memoria, te devolvera el valor del return
     }
     
     
